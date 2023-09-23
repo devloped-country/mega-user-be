3a26e119b75ef74d82a54c99ffeefa860199546d
shshe22 <shshe22@naver.com>
2023-09-23 19:23:47 +0900

    design: 수정

--- src/main/java/com/mega/biz/notice/model/NoticeDAO.java
+++ src/main/java/com/mega/biz/notice/model/NoticeDAO.java
@@ -54,7 +54,7 @@ public class NoticeDAO {
                 noticeDTO.setTitle(rs.getString("TITLE"));
                 noticeDTO.setContent(rs.getString("CONTENT"));
                 noticeDTO.setAuthor(rs.getString("AUTHOR"));
-                noticeDTO.setCreatedDate(rs.getString("CREATEDDATE"));
+                noticeDTO.setCreatedDate(String.valueOf(rs.getDate("CREATED_DATE")));
 
                 noticeList.add(noticeDTO);
             }
@@ -84,7 +84,7 @@ public class NoticeDAO {
                 noticeDTO.setTitle(rs.getString("TITLE"));
                 noticeDTO.setContent(rs.getString("CONTENT"));
                 noticeDTO.setAuthor(rs.getString("AUTHOR"));
-                noticeDTO.setCreatedDate(rs.getString("CREATEDDATE"));
+                noticeDTO.setCreatedDate(String.valueOf(rs.getDate("CREATED_DATE")));
                 noticeDTO.setTag_id(rs.getString("TAG_ID"));
             }
 
