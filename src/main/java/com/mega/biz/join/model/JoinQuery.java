d5c7d304062655dc3fed1e06fb7e094262f08d14
Chanhyun Wang <rubberramen@naver.com>
2023-09-24 15:20:26 +0900

    comment : 코멘트 삭제

--- src/main/java/com/mega/biz/join/model/JoinQuery.java
+++ src/main/java/com/mega/biz/join/model/JoinQuery.java
@@ -3,7 +3,6 @@ package com.mega.biz.join.model;
 public enum JoinQuery {
     USER_INSERT("insert into user3 (email, password, name, phone, user_status, salt) values(?, ?, ?, ?, ?, ?)"),
     USER_FIND("select * from user3 where email = ?"),
-
     INSERT_ATTENDANCE("insert into attendance2 (attendance_stat, start_date, end_date, email) values (1, now(), now(), ?)");
 
     private final String query;
