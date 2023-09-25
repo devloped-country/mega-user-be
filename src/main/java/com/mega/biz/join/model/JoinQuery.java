9affd5417d2e77753e672140710b21c5cc4c1173
Chanhyun Wang <rubberramen@naver.com>
2023-09-25 10:44:48 +0900

    fix : 회원 가입시 attendance 테이블 시간00:00:00으로 입력

--- src/main/java/com/mega/biz/join/model/JoinQuery.java
+++ src/main/java/com/mega/biz/join/model/JoinQuery.java
@@ -3,7 +3,7 @@ package com.mega.biz.join.model;
 public enum JoinQuery {
     USER_INSERT("insert into user3 (email, password, name, phone, user_status, salt) values(?, ?, ?, ?, ?, ?)"),
     USER_FIND("select * from user3 where email = ?"),
-    INSERT_ATTENDANCE("insert into attendance2 (attendance_stat, start_date, end_date, email) values (1, now(), now(), ?)");
+    INSERT_ATTENDANCE("insert into attendance2 (attendance_stat, start_date, end_date, email) values (1, TIMESTAMP(CURDATE(), MAKETIME(0,0,0)), TIMESTAMP(CURDATE(), MAKETIME(0,0,0)), ?)");
 
     private final String query;
 
